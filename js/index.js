f5cd72c30e532939f9fd6ef4e9a216ac950fc9ae
arish998 <72273375+arish998@users.noreply.github.com>
2021-05-03 23:08:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,10 +294,9 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-ifrm.setAttribute("src", "https://www.youtube.com/embed/WKAsCvpJqUI?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/WKAsCvpJqUI?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
   document.querySelector('#video').appendChild(ifrm);
 }
-
